0311a7593d65f778807914daf8dd5613391b8e26
icoder-new <ehsonjonazimi05@gmail.com>
2023-10-12 23:09:22 +0500

    added to msg parsed data from json

--- utils/smtp.go
+++ utils/smtp.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+	"fr33d0mz/goMail/internal"
 	"net/smtp"
 )
 
@@ -13,12 +15,24 @@ func SendMessage(email, firstname, subject, message string) error {
 		"We'll answer you as soon as possible!\n\n" +
 		"Here is your message:\n\n" +
 		message + "\n\n" +
-		"Here is currency realtime:\n\n"
-		// + "wtf to write?"
+		"Here is currency realtime:\n\n" +
+		currencyData()
 
 	err := smtp.SendMail(AppSettings.SMTPParams.Server+":"+AppSettings.SMTPParams.Port,
 		smtp.PlainAuth("", AppSettings.SMTPParams.Username, AppSettings.SMTPParams.Password, AppSettings.SMTPParams.Server),
 		AppSettings.SMTPParams.Username, []string{email}, []byte(msg))
 
 	return err
+}
+
+func currencyData() string {
+	msg := "Transfer\t\tTrade\t\tValue\t\tDate\t\tName\n\n"
+
+	data, _ := internal.GetJSONData()
+
+	for _, rate := range data.LocalRates {
+		msg += fmt.Sprintf("%s\t\t%s\t\t%s\t\t%s\t\t%s\n\n", rate.MoneyTransferBuyValue, rate.MoneyTransferTradeValue, rate.SellValue, rate.CreatedAt, rate.Name)
+	}
+
+	return msg
 }
